d26b9bb52ee58fb91eaadaf9a141ac4c916298b1
Jayme Anchante <jayme.anchante@disroot.org>
2019-10-07 20:23:31 -0300

    ES6+ classes

---
+++ 2_es6_course/src/anotherMySum.js
@@ -0,0 +1,3 @@
+export default function anotherMySum(a, b) {
+    return a + b;
+};
---
+++ 2_es6_course/src/functions.js
@@ -0,0 +1,7 @@
+export function myfunctionsum(a, b) {
+    return a + b;
+};
+
+export function myfunctionsubtraction(a, b) {
+    return a - b;
+};
