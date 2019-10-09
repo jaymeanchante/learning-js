5377912bcf322e840d4ef40ba02e1155561b62cc
Jayme Anchante <jayme.anchante@disroot.org>
2019-10-09 09:15:54 -0300

    finished the routes shell script

--- 3_nodejs/curl.sh
+++ 3_nodejs/curl.sh
@@ -3,27 +3,25 @@
 base_url="http://localhost:3001/api"
 
 if [ $1 = "index" ]; then
-    curl -s -X GET $base_url/products | jq .
+    curl -s -X GET $base_url/products?page=$2 | jq .
 elif [ $1 = "show" ]; then
     curl -s -X GET $base_url/products/$2 | jq .
 elif [ $1 = "store" ]; then
     curl -s -X POST -H "Content-Type: application/json" $base_url/products -d '
     {
-        "title": "$2",
-        "description": "$3",
-        "url": "$4"
+        "title": "'"$2"'",
+        "description": "'"$3"'",
+        "url": "'"$4"'"
     }' | jq .
 elif [ $1 = "update" ]; then
-    curl -s -X POST -H "Content-Type: application/json" $base_url/products/$2 -d '
+    KEY=$(echo $@ | cut -f3 -d- | cut -f1 -d=)
+    VALUE=$(echo $@ | cut -f3 -d- | cut -f2 -d=)
+    curl -s -X PUT -H "Content-Type: application/json" $base_url/products/$2 -d '
     {
-        "title": "$2",
-        "description": "$3",
-        "url": "$4"
-    }'
+        "'"$KEY"'": "'"$VALUE"'"
+    }' | jq .
 elif [ $1 = "destroy" ]; then
-    echo "a"
-elif [ $1 = "other" ]; then
-    echo $@
+    curl -s -X DELETE -H "Content-Type: application/json" $base_url/products/$2 | jq .
 else
-    echo "use index, show or store!"
+    echo "use index, show, store, update or destroy!"
 fi
