b5cf2151ef5e6e55b0bd16e9a2894cdf3befce10
Jayme Anchante <jayme.anchante@disroot.org>
2019-10-10 22:56:40 -0300

    last classes

--- 4_reactjs/src/App.js
+++ 4_reactjs/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './styles.css'
+import Header from './components/Header';
+import Main from './pages/main';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
 
 export default App;
